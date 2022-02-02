45e9337e4f8f36ed11e2c7b0aa33a11ad1a299ff
Edson Junior <edsonjn2013@gmail.com>
2022-02-02 00:58:46 -0300

    feat: create jwt token

--- src/app/controller/authController.js
+++ src/app/controller/authController.js
@@ -1,13 +1,16 @@
 /* eslint-disable consistent-return */
 /* eslint-disable class-methods-use-this */
 const bcrypt = require('bcryptjs');
+const gerarToken = require('../utils/authentication');
 const authService = require('../service/authService');
+
+
 
 class AuthController{
 
     async authenticateUser(req,res){
         const {email , senha} = req.body;
-
+        
         try {
             
             const authUser = await authService.findAuthenticate({email});
@@ -22,7 +25,11 @@ class AuthController{
 
             authUser.senha = undefined;
 
-            res.send(authUser);    
+            res.send({
+                authUser,
+                token: gerarToken({ id: authUser.id })
+            });    
+
         } catch (error) {
             return res.status(400).json({error});
         }
---
+++ src/app/utils/authentication.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
+function gerarToken(params = {}){
+    return jwt.sign({params},authConfig.secret,{
+        expiresIn: 86400,
+    });
+}
+
+module.exports = gerarToken;
