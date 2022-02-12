174b19a67bd2c091fca2668773cb5b2b639b1949
Edson Junior <edsonjn2013@gmail.com>
2022-02-11 21:51:00 -0300

    ref: update schema rental

--- src/app/schema/rentalSchema.js
+++ src/app/schema/rentalSchema.js
@@ -29,13 +29,36 @@ const RentalSchema = mongoose.Schema({
         maxLength: 8,
         required:true},
 
-      number: {
-        type:String, 
-        required:true},
+      logradouro:{
+        type:String,
+        required:true
+      },
 
       complemento: {
-        type: String},
+        type: String
+      },
 
+      bairro:{
+        type: String,
+        required:true
+      },
+
+      number: {
+        type:String, 
+        required:true
+      },
+
+      localidade:{
+        type:String,
+        required:true
+      },
+
+      uf:{
+        type: String,
+        minLength: 2,
+        required:true
+      },
+    
       isFilial:{
         type: Boolean, 
         required:true,
