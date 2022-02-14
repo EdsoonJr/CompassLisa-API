59fb3b52a668ea7c90d30c524e8cc054e5b17e41
Edson Junior <edsonjn2013@gmail.com>
2022-02-14 17:19:13 -0300

    feat: validation put rental

--- src/routes/rentalRoute.js
+++ src/routes/rentalRoute.js
@@ -1,12 +1,13 @@
 const rentalController = require('../app/controller/rentalController');
 const validateInsertRental = require('../app/validation/rental/insertRental');
 const validateIdRental = require('../app/validation/id/id');
+const validatePutRental = require('../app/validation/rental/putRental');
 
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
   routes.post('/', validateInsertRental, rentalController.insertRental);
   routes.get('/', rentalController.getAllRentals);
   routes.get('/:id', validateIdRental, rentalController.findOneRental);
-  routes.put('/:id', validateIdRental, rentalController.updateRental);
+  routes.put('/:id', validateIdRental, validatePutRental, rentalController.updateRental);
   routes.delete('/:id', validateIdRental, rentalController.deleteRental);
   server.use(prefix, routes);
   
