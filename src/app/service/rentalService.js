23d3d7347a35cad41ae59c4407249765f4cd2781
Edson Junior <edsonjn2013@gmail.com>
2022-02-14 19:07:20 -0300

    feat: add unique isFilial False.

--- src/app/service/rentalService.js
+++ src/app/service/rentalService.js
@@ -5,10 +5,10 @@ class RentalService{
 
   async create(payload, data){
 
+    const inputCep = payload.endereco;
     for (let index = 0; index < payload.endereco.length; index++) {
       
-      const ceps = payload.endereco;
-      const address = ceps[index];
+      const address = inputCep[index];
       const dados = await ViaCepApi.findCep(address.cep);
       const { cep, logradouro, complemento, bairro, localidade, uf } = dados;
 
@@ -19,7 +19,18 @@ class RentalService{
       address.localidade = localidade;
       address.uf = uf;  
     }
-  
+    
+    const isFilial = inputCep.filter((value) => value.isFilial !== true);
+
+    if( isFilial.length > 1 ){
+      throw new Error('The rental company needs a matrixxx');
+
+    }else if (isFilial.length === 0 )   {
+      throw new Error('More than one matrix is not allowed in the rental company');
+    } 
+
+
+
     const newRental = await rentalRepository.create(payload, data);
     return newRental;
   }
