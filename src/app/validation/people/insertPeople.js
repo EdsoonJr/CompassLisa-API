dde8d023f514b89a950a42353b7abe483b0b5454
Edson Junior <edsonjn2013@gmail.com>
2022-02-14 23:09:56 -0300

    ref: separating the validation function from cpf

--- src/app/validation/people/insertPeople.js
+++ src/app/validation/people/insertPeople.js
@@ -1,51 +1,25 @@
 
 const Joi = require('joi').extend(require('@joi/date'));
+const isValidCpf = require('../../utils/validates/isValidCpf');
+const BadRequest = require('../../utils/Errors/BadRequest');
 
 
 module.exports = async (req, res, next)=>{
   
   try {
-    function isValidCPF(cpf) {
-      if (typeof cpf !== "string") return false;
-      cpf = cpf.replace(/[\s.-]*/igm, '');
-      if (
-        !cpf ||
-                cpf.length !== 11 ||
-                cpf === "00000000000" ||
-                cpf === "11111111111" ||
-                cpf === "22222222222" ||
-                cpf === "33333333333" ||
-                cpf === "44444444444" ||
-                cpf === "55555555555" ||
-                cpf === "66666666666" ||
-                cpf === "77777777777" ||
-                cpf === "88888888888" ||
-                cpf === "99999999999" 
-      ) {
-        return false;
-      }
-      let soma = 0;
-      let resto;
-      for (var i = 1; i <= 9; i++) 
-        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
-      resto = (soma * 10) % 11;
-      if ((resto === 10) || (resto === 11))  resto = 0;
-      if (resto !== parseInt(cpf.substring(9, 10)) ) return false;
-      soma = 0;
-      for (var i = 1; i <= 10; i++) 
-        soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
-      resto = (soma * 10) % 11;
-      if ((resto === 10) || (resto === 11))  resto = 0;
-      if (resto !== parseInt(cpf.substring(10, 11) ) ) return false;
-      return true;
-    }
-       
+   
     const schema = Joi.object({
       nome: Joi.string().min(3).required(),
 
       data_nascimento: Joi.date().format('DD/MM/YYYY').less('01-01-2004').max('now').required(),
 
-      cpf: Joi.string().min(11).max(11).required(),
+      cpf: Joi.string().min(11).max(11).required().custom((value)=>{
+        if(!isValidCpf(value)){
+          throw new BadRequest(`Invalid CPF ${value}`);
+        }else{
+          return true;
+        }
+      }).message(`Invalid CPF ${req.body.cpf}`),
 
       email: Joi.string().email().required(),
 
@@ -61,13 +35,6 @@ module.exports = async (req, res, next)=>{
       throw   {
         'description': error.details[0].path[0],
         'name':error.message
-      };
-    }
-
-    if(!isValidCPF(req.body.cpf)){
-      throw {
-        'description': 'Bad Request',
-        'name': `Invalid CPF ${req.body.cpf}`
       };
     }
         
