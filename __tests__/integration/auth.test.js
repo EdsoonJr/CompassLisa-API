80ce9f8e42db158a562cf971b24979d665107c75
Edson Junior <edsonjn2013@gmail.com>
2022-02-20 15:55:27 -0300

    test: finalized tests in auth

--- __tests__/integration/auth.test.js
+++ __tests__/integration/auth.test.js
@@ -29,4 +29,36 @@ describe('Test All Routes Auths', () => {
     });
     expect(result.statusCode).toBe(200);
   });
+
+  it('Test Route  Post *INVALID EMAIL*', async () => {
+    const result = await supertest(app).post('/api/v1/authenticate').send({
+      email: 'juinimtemail.com',
+      senha: '654321'
+    });
+    expect(result.statusCode).toBe(400);
+  });
+
+  it('Test Route  Post *INVALID SENHA*', async () => {
+    const result = await supertest(app).post('/api/v1/authenticate').send({
+      email: 'juinimtk2@email.com',
+      senha: '65'
+    });
+    expect(result.statusCode).toBe(400);
+  });
+
+  it('Test Route  Post *NOT FOUND*', async () => {
+    const result = await supertest(app).post('/api/v1/authenticate').send({
+      email: 'notfound@email.com',
+      senha: '654321'
+    });
+    expect(result.statusCode).toBe(404);
+  });
+
+  it('Test Route  Post *NO EMPTY*', async () => {
+    const result = await supertest(app).post('/api/v1/authenticate').send({
+      email: '',
+      senha: ''
+    });
+    expect(result.statusCode).toBe(400);
+  });
 });
